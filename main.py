f90bdb135d0548b84c4f3215a11965d2fce5d3d4
IT-workers713 <rayehriad99@gmail.com>
2024-02-01 23:47:15 +0100

    day 27 app-news-api send email with titles and descriptions

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-
+from send_email import send_email
 url = ('https://newsapi.org/v2/top-headlines?'
        'sources=bbc-news&'
        'apiKey=4c0d1225cfd14c289c4daa2bdd448d11')
@@ -9,7 +9,14 @@ request= requests.get(url)
 #get dectionnnary
 
 content = request.json()
-#aaccess the article titles
-for article in content["articles"]:
-       print(article["title"])
 
+#access the article titles
+body = ""
+for article in content["articles"]:
+       #if there is an error check if article title is none
+       if article["title"] is not None:
+              body = body + article["title"] + "\n" + article["description"] + 2 * "\n"
+
+
+body=body.encode("utf-8")
+send_email(message=body)
