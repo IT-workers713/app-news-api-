df1b56f34fd61ff6ac5c04a730e611f47b4364e5
IT-workers713 <rayehriad99@gmail.com>
2024-02-01 23:58:43 +0100

    day 28 ADD LINKS SPECIFY LANGUAGE LIMIT NEWS ADD SUBJECT CONTROL CATEGORY

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import requests
 from send_email import send_email
+topic = "tesla"
 url = ('https://newsapi.org/v2/top-headlines?'
-       'sources=bbc-news&'
-       'apiKey=4c0d1225cfd14c289c4daa2bdd448d11')
+       f"q={topic}&"
+       'apiKey=4c0d1225cfd14c289c4daa2bdd448d11'
+       '&language=en')
 #make request
 
 request= requests.get(url)
@@ -12,10 +14,10 @@ content = request.json()
 
 #access the article titles
 body = ""
-for article in content["articles"]:
+for article in content["articles"][:20]:
        #if there is an error check if article title is none
        if article["title"] is not None:
-              body = body + article["title"] + "\n" + article["description"] + 2 * "\n"
+              body = "Subject : Today's news "+"\n"+body + article["title"] + "\n" + article["description"] +"\n"+article["url"]+ 2 * "\n"
 
 
 body=body.encode("utf-8")
